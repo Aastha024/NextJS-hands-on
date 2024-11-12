73383f67ecd33c191ef780c76c170bdb6d3bf09d
Aastha024 <aastha.shah@seaflux.tech>
2024-11-12 19:04:23 +0530

    increment id

--- src/app/api/addUser/route.ts
+++ src/app/api/addUser/route.ts
@@ -1,22 +1,25 @@
 import fs from 'fs';
 interface User {
+  id: number;
   name: string;
   email: string;
 }
+let ids = 1;
 
 export async function POST(request: Request): Promise<Response> {
   
-  const { name, email } = await request.json() as { name: string; email: string };
+  const {name, email } = await request.json() as { name: string; email: string };
 
   let users: User[] = [];
+
   try {
     const data = fs.readFileSync('users.json', 'utf-8');
     users = JSON.parse(data);
   } catch (error) {
-    console.log('error', error);
+    console.log('errors', error);
   }
 
-  users.push({ name, email });
+  users.push({ id: ids++, name, email });
 
   fs.writeFileSync('users.json', JSON.stringify(users, null, 2), 'utf-8');
 
